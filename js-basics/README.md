eaf5345527248415ba7a3471a695f4bb5aad82c7
damarismuhia <muhia.damaris@eclectics.io>
2026-01-20 21:28:05 +0300

    filter, map, reduce

--- js-basics/README.md
+++ js-basics/README.md
@@ -3,10 +3,10 @@
 2. alt/ctl + up/down - move line of code
 
 
-# JavaScript
+# 1. JavaScript
 JS is dynamically-typed language. We don't explicitly define the types for our variables
 
-# Operators
+# 2. Operators
 
 1. Spread Operator
 - we use the ...spread operator to take in the values from the array. eg [1,2,3] results to 1,2,3
@@ -104,5 +104,10 @@ or
 ((params)=> {body})(params)
 
 ```
+
+# Loops
+- Starndard loops - for , while, do while loops
+- for-of cleaner way to iterate thru an array compared to above 3. It gives the actual value
+- for-in - gives the index/key of a given array & object respectively
 
 
--- loops/loops.js
+++ loops/loops.js
@@ -222,3 +222,145 @@ const filteredData = customerData.filter((item, index, array) => item.customerNa
 console.log(filteredData);
 
 //Mark: b) Array.map()
+console.log('\n\n.map() takes in a callback function with params: and returns the modified data');
+const int = [1, 2, 3, 5, 6]
+const finalMappedInt = int.map((number) => number * 9 + '')
+console.log(finalMappedInt);
+
+    //b i) create an array of company names from companies array
+   var companies = [
+    {
+        id: 1,
+        name: "BokPay Ltd",
+        industry: "FinTech",
+        employees: 120,
+        country: "Kenya",
+        isActive: true
+    },
+    {
+        id: 2,
+        name: "EcoSoft Solutions",
+        industry: "Software",
+        employees: 45,
+        country: "Uganda",
+        isActive: true
+    },
+    {
+        id: 3,
+        name: "HealthPlus Africa",
+        industry: "HealthTech",
+        employees: 200,
+        country: "Rwanda",
+        isActive: false
+    },
+    {
+        id: 4,
+        name: "AgroLink Systems",
+        industry: "AgriTech",
+        employees: 78,
+        country: "Tanzania",
+        isActive: true
+    }
+    ];
+
+    //array of company names
+    const companyNames = companies.map((company) => company.name)
+    console.log(companyNames);
+
+    //An array of object with company and industry
+    const companyCategory = companies.map((company) =>  {
+       return {
+            company: company.name,
+            industry: company.industry
+        };
+    });
+    console.log(companyCategory);
+
+    //Chain Map Methods
+    const squareNDouble = int
+        .map((number) => Math.floor(Math.sqrt(number)))
+        .map((sqredNumber) => sqredNumber * 3)
+        .filter(function (value) {
+            return value % 2 === 0
+        })
+    console.log(squareNDouble);
+
+//mark 3. reduce - previousValue,accumulator, current value
+const totalEmployees = companies.reduce( 
+    function(accumulator, companyObj) {
+        return accumulator + companyObj.employees
+    },0//initialValue
+)
+console.log('\n\n .reduce(): ', 'Total Employees: ', totalEmployees);
+
+/**
+ * # Array Method Challenges
+
+## Challenge 1
+
+**Instructions:**
+Take the `people` array and create an array called `youngPeople` 
+that stores objects with ONLY `name` and `email` properties of all the people that are 25 and under.
+ The `name` property should have their first and last name.
+ */
+const people = [
+  {
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'john@gmail.com',
+    phone: '111-111-1111',
+    age: 30,
+  },
+  {
+    firstName: 'Jane',
+    lastName: 'Poe',
+    email: 'jane@gmail.com',
+    phone: '222-222-2222',
+    age: 25,
+  },
+  {
+    firstName: 'Bob',
+    lastName: 'Foe',
+    email: 'bob@gmail.com',
+    phone: '333-333-3333',
+    age: 45,
+  },
+  {
+    firstName: 'Sara',
+    lastName: 'Soe',
+    email: 'Sara@gmail.com',
+    phone: '444-444-4444',
+    age: 19,
+  },
+  {
+    firstName: 'Jose',
+    lastName: 'Koe',
+    email: 'jose@gmail.com',
+    phone: '555-555-5555',
+    age: 23,
+  },
+];
+
+const youngPeople = people
+.filter((objc) => objc.age <= 25)
+.map((obj) => {
+    return {
+        name: `${obj.firstName} ${obj.lastName}`,
+        email: obj.email
+    }
+});
+
+
+console.log(youngPeople);
+
+/**
+ * ## Challenge 2
+
+**Instructions:**
+
+Add all of the positive numbers in the array.
+
+ */
+const nums = [2, -30, 50, 20, -12, -9, 7];
+const positiveNums = nums
+.filter((num) => num > 0)
